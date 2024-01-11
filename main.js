49fc556001401f46fd4ceb206dd80d981faa1dcf
Carlos Campbell <carlos.g.campbell@gmail.com>
2024-01-10 23:42:58 -0500

    full kata

---
+++ main.js
@@ -0,0 +1,10 @@
+/**
+ Very simple, given an integer or a floating-point number, find its opposite.
+  */
+
+
+ function opposite(number) {
+    return number * -1
+  }
+
+  console.log(opposite(3))
